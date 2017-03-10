2b1e9a9d3ddf0947d40bbe610e535478faadf50e
Rubén Díaz Martínez <rubendm92@gmail.com>
2017-03-10 18:39:08 +0100

    Refactor to calculate discounts with multimethod

--- src/books_basket/core.clj
+++ src/books_basket/core.clj
@@ -3,12 +3,11 @@
 
 (defrecord Book [genre price])
 
-(def discounts
-  {
-   :fantasy (fn [number-of-books base-price] (* 0.8 base-price))
-   :it (fn [number-of-books base-price] (if (> number-of-books 2) (* 0.7 base-price) (* 0.9 base-price)))
-   :travel (fn [number-of-books base-price] (if (> number-of-books 3) (* 0.6 base-price) base-price))
-   })
+(defmulti apply-discount (fn [[genre [number-of-books base-price]]] genre))
+(defmethod apply-discount :fantasy [[_ [_ base-price]]] (* 0.8 base-price))
+(defmethod apply-discount :it [[_ [number-of-books base-price]]] (if (> number-of-books 2) (* 0.7 base-price) (* 0.9 base-price)))
+(defmethod apply-discount :travel [[_ [number-of-books base-price]]] (if (> number-of-books 3) (* 0.6 base-price) base-price))
+(defmethod apply-discount :default [[_ [_ base-price]]] base-price)
 
 (defn- calculate-base-price [books] (reduce + (map :price books)))
 
@@ -17,10 +16,6 @@
 
 (defn- price-by-book-genre [books] (map genre-entry (group-by :genre books)))
 
-(defn- apply-discount [[genre [number-of-books base-price]]]
-  ((get discounts genre (fn [number-of-books base-price] base-price)) number-of-books base-price))
-
 (defn- calculate-price [books] (map apply-discount (price-by-book-genre books)))
 
 (defn total [books] (reduce + (calculate-price books)))
-
